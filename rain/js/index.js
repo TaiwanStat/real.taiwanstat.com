accfa9d9d5b474ff984250f0f3fd05af493aad22
chihsuan <chihsuan.huang.7@gmail.com>
2015-10-04 00:31:37 +0800

    update

--- rain/js/index.js
+++ rain/js/index.js
@@ -15,10 +15,16 @@
       '連江縣', '金門縣','澎湖縣'
   ];
   
+  var myFirebaseRef = new Firebase("https://realtaiwanstat2.firebaseio.com");
+
   init();
   function init() {
     time = (now > 5 && now < 18) ? '白天' : '晚上';
-    $.getJSON('./data/data.json').then(function(_data) {
+    var myFirebaseRef = new Firebase("https://realtaiwanstat2.firebaseio.com");
+    myFirebaseRef.child("rain").limitToLast(1).on("child_added", function(snapshot) {
+      var raw = snapshot.val();  
+      var _data = JSON.parse(raw);
+    //$.getJSON('./data/data.json').then(function(_data) {
       data = _data;
       $('.updateAt').text(data[0].PublishTime);
       sumCountryData();
