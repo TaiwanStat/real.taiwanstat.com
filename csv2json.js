53ef7b501201b370535d01cab799b9668fc83b12
chihsuan <chihsuan.tw@gmail.com>
2015-12-21 02:18:35 +0800

    update movie

--- csv2json.js
+++ csv2json.js
@@ -1,3 +1,4 @@
 var csvjson = require('csvjson');
 csvjson.toObject('./uv/data/data.csv').save('./uv/data/data.json');
+csvjson.toObject('./data/movie_location.csv').save('./data/movie_location.json');
 csvjson.toObject('./gamma/data/gammamonitor.csv').save('./gamma/data/gammamonitor.json');
