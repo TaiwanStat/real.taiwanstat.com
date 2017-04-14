c0ceb90c6c7250ac559afaf51c4ce74f70ffb863
rapirent <rapirent@gmail.com>
2017-04-14 21:33:15 +0800

    add package.json

--- river/js/index.js
+++ river/js/index.js
@@ -1,7 +1,7 @@
 (function() {
 
   var map;
-  var siteInfo; 
+  var siteInfo;
   var riverData;
   var geojson; // basin layer
 
@@ -52,38 +52,38 @@
 
       for (var i in siteInfo) {
         if(siteInfo[i].SiteName in riverData) {
-          riverData[siteInfo[i].SiteName]['RPI'] = 
+          riverData[siteInfo[i].SiteName]['RPI'] =
             parseFloat(riverData[siteInfo[i].SiteName]['RPI']);
           if (isNaN(riverData[siteInfo[i].SiteName]['RPI'])) {
             continue;
           }
 
           var polluDegree = getPollutionDegree(riverData[siteInfo[i].SiteName]['RPI']);
-          var icon =  L.AwesomeMarkers.icon({prefix: 'fa', icon: 'map-marker', 
+          var icon =  L.AwesomeMarkers.icon({prefix: 'fa', icon: 'map-marker',
             markerColor: polluDegree.color});
 
           var marker = L.marker([
             siteInfo[i].TWD97Lat, siteInfo[i].TWD97Lon],
             {icon: icon, opacity: 0.9})
-            .addTo(map);   
+            .addTo(map);
 
           /*marker.on('click', function(e) {
             map.setView(this.getLatLng(), 11);
           });*/
 
-          marker.bindPopup('<strong>測站名稱：' + siteInfo[i].SiteName 
+          marker.bindPopup('<strong>測站名稱：' + siteInfo[i].SiteName
                       + '</strong><br/><span class="red">污染程度：' + polluDegree.disc
-                      + "</span><br/>所屬流域：" + siteInfo[i].Basin 
+                      + "</span><br/>所屬流域：" + siteInfo[i].Basin
                       + '<br/>RPI指標：' + riverData[siteInfo[i].SiteName].RPI
                       + '<br/>酸鹼值：' + riverData[siteInfo[i].SiteName].pH
-                      + '<br/>懸浮固體：' 
+                      + '<br/>懸浮固體：'
                       + riverData[siteInfo[i].SiteName].SS + '（mg/L）'
-                      + '<br/>溶氧量：' 
+                      + '<br/>溶氧量：'
                       + riverData[siteInfo[i].SiteName].DO + '（mg/L）'
-                      + '<br/>生化需氧量：' 
+                      + '<br/>生化需氧量：'
                       + riverData[siteInfo[i].SiteName].COD + '（mg/L）'
                       + '<br/>氨氮：' + riverData[siteInfo[i].SiteName].NH3N + '（mg/L）'
-                      + '<br/>地址：'+ siteInfo[i].SiteAddress); 
+                      + '<br/>地址：'+ siteInfo[i].SiteAddress);
 
           if (siteInfo[i].Basin in basinRPI) {
             basinRPI[siteInfo[i].Basin].RPI += riverData[siteInfo[i].SiteName]['RPI'];
@@ -99,7 +99,7 @@
       for (var i in basinRPI) {
         basinRPI[i].RPI = parseFloat(basinRPI[i].RPI) / basinRPI[i].siteNumber;
       }
-   
+
       geojson = L.geoJson(geoData, {
         style: style,
         onEachFeature: onEachFeature
@@ -121,20 +121,20 @@
       return { disc: '中度污染', color: 'red'};
     }
     else {
-      return { disc: '嚴重污染', color: 'darkpurple'}; 
+      return { disc: '嚴重污染', color: 'darkpurple'};
     }
   }
 
   function initMap() {
     map = new L.Map('map');
 
-    var url = 'https://{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png';
+    var url = 'http://{s}.osm.maptiles.xyz/{z}/{x}/{y}.png';
     var attrib = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
-    var osm = new L.TileLayer(url, {minZoom: 1,  maxZoom: 16, attribution: attrib});   
+    var osm = new L.TileLayer(url, {minZoom: 1,  maxZoom: 16, attribution: attrib});
 
     map.setView(new L.LatLng(23.7, 121), 8);
     osm.addTo(map);
-    info.addTo(map); 
+    info.addTo(map);
     legend.addTo(map);
   }
 
@@ -147,7 +147,7 @@
         dashArray: '3',
         fillOpacity: 0.4
     };
-  } 
+  }
 
   function getColor(name) {
     var d = 0;
@@ -212,7 +212,7 @@
     }
     else {
      this._div.innerHTML = '<h4>河川流域名稱：' +  (props ?
-         props.name + '</h4>流域內無測站資料' 
+         props.name + '</h4>流域內無測站資料'
          : '</h4>請點選畫面區塊' );
     }
   };
@@ -226,7 +226,7 @@
       var labels = ['嚴重污染（6+）', '中度污染（3-6）', '輕度污染（2-3）', '未（稍）受污染（0-2）'];
 
       for (var i = 0; i < labels.length; i++) {
-          div.innerHTML += '<i style="background:' + color[i] 
+          div.innerHTML += '<i style="background:' + color[i]
             + '"></i>' + labels[i] + '<br/>';
       }
 
@@ -243,7 +243,7 @@
   }
 
   function setPosition(position) {
-    map.setView(new L.LatLng(position.coords.latitude, 
+    map.setView(new L.LatLng(position.coords.latitude,
           position.coords.longitude), 12);
   }
 
@@ -253,19 +253,19 @@
 
   function toogleLinks() {
     if (showLinks) {
-      $('.links').hide(); 
+      $('.links').hide();
     }
     else {
-      $('.links').show(); 
+      $('.links').show();
     }
     showLinks = !showLinks;
   }
 
   function showRPIInfo() {
-      $('.rpi-info').show(); 
+      $('.rpi-info').show();
   }
   function hideRPIInfo() {
-      $('.rpi-info').hide(); 
+      $('.rpi-info').hide();
   }
 
 })();
--- river/river.js
+++ river/river.js
@@ -6,63 +6,64 @@ var fs = require('fs');
 
 var _URL = 'http://edw.epa.gov.tw/reportInspectRiver.aspx';
 var params = {
-  county: '全國',
-  river: '全國',
-  area: '全國',
-  drain: '全國',
-  date: '201503'
+    county: '全國',
+    river: '全國',
+    area: '全國',
+    drain: '全國',
+    date: '201612'
 };
 
 getRiverData();
 
-function getRiverData () {
-  async.waterfall([
-    function (){
-      request.post({url: _URL, form: params}, function (error, response, body) {
-        if(error){
-          console.error('Request error.');
+function getRiverData() {
+    async.waterfall([
+        function() {
+            request.post({ url: _URL, form: params }, function(error, response, body) {
+                if (error) {
+                    console.error('Request error.');
+                }
+                var outputData = parseData(body);
+                fs.writeFile('river.json', JSON.stringify(outputData));
+                console.log('output: river.json');
+            })
         }
-        var outputData = parseData(body);
-        fs.writeFile('river.json', JSON.stringify(outputData));
-        console.log('output: river.json');
-      }
-      )}
-    ], function (err, outputData) {
-      if (err) {
-        console.error('Error.');
-      }
+    ], function(err, outputData) {
+        if (err) {
+            console.error('Error.');
+        }
 
-      if (!outputData || outputData.length === 0) {
-        console.log('GET NULL');
-      }
-   });
+        if (!outputData || outputData.length === 0) {
+            console.log('GET NULL');
+        }
+    });
 };
 
-function parseData (html){
+function parseData(html) {
 
-  var outputData = {};
-  var $ = cheerio.load(html);
-  var attr = ['siteName', 'date', 'RPI', 'WT', 'PH', 'EC', 'Temperature', 
-    'SS', 'DO', 'BOD5', 'COD', 'NH3N', 'Cl', 'DOS', 'Coliform', 'Cd', 'Pb', 'Cu',
-    'Zn', 'Cr', 'Mn', 'Ag', 'As', 'Se'];
+    var outputData = {};
+    var $ = cheerio.load(html);
+    var attr = ['siteName', 'date', 'RPI', 'WT', 'PH', 'EC', 'Temperature',
+        'SS', 'DO', 'BOD5', 'COD', 'NH3N', 'Cl', 'DOS', 'Coliform', 'Cd', 'Pb', 'Cu',
+        'Zn', 'Cr', 'Mn', 'Ag', 'As', 'Se'
+    ];
 
-  $('#GroupingGridView1')
-    .find('tr.DataTableRowStyle, tr.DataTableAlternatingRowStyle')
-    .each(function (number, elem){
-      var tds = $(this).find('td');
-      var tdBasic = tds.length - 27;
-      var siteData = {};
-      var siteName =  $(this).find('td').eq(tdBasic)
-                      .text().trim().replace(/(\r\n|\n|\r|\s)/g,'');
-      
-      for (var i = tdBasic; i < (tdBasic+15); ++i) {
-        siteData[attr[i-tdBasic]] = $(this).find('td')
-                                   .eq(i)
-                                   .text()
-                                   .trim()
-                                   .replace(/(\r\n|\n|\r|\s)/g,'');
-      }
-      outputData[siteName] = siteData;
-  });
-  return outputData;
+    $('#GroupingGridView1')
+        .find('tr.DataTableRowStyle, tr.DataTableAlternatingRowStyle')
+        .each(function(number, elem) {
+            var tds = $(this).find('td');
+            var tdBasic = tds.length - 27;
+            var siteData = {};
+            var siteName = $(this).find('td').eq(tdBasic)
+                .text().trim().replace(/(\r\n|\n|\r|\s)/g, '');
+
+            for (var i = tdBasic; i < (tdBasic + 15); ++i) {
+                siteData[attr[i - tdBasic]] = $(this).find('td')
+                    .eq(i)
+                    .text()
+                    .trim()
+                    .replace(/(\r\n|\n|\r|\s)/g, '');
+            }
+            outputData[siteName] = siteData;
+        });
+    return outputData;
 }
